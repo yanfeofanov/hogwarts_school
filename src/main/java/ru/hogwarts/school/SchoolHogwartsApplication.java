70c2bde0922fd911e3ae20f31dea7b4b25c30cee
kaVII-x <ka52jiguli@yandex.ru>
2023-06-19 18:55:19 +0400

    version 1.1

---
+++ src/main/java/ru/hogwarts/school/SchoolHogwartsApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolHogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolHogwartsApplication.class, args);
+    }
+
+}
